d3aa8c6ee00949c253aaedeae22d864b94a6dbcf
elvis <cenelvis@gmail.com>
2022-06-17 21:57:57 -0400

    Using Routes, { Route, BrowserRouter }

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import { Route } from "react-router-dom"; // Needs to be activated with { BrowserRouter } in top layer
+import Welcome from "./pages/Welcome";
+import Product from "./pages/Product";
+
+// localhost:3000/welcome
+// localhost:3000/product
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <Route path="/welcome">
+        <Welcome />
+      </Route>
+      <Route path="/product">
+        <Product />
+      </Route>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
-import ReactDOM from 'react-dom/client';
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom"; // Activates { Route } in "App.js"
 
-import './index.css';
-import App from './App';
+import "./index.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// Activates { Route } in "App.js"
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
