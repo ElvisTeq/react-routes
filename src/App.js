1cb145d2f6ce19430bd9d410d7cfbaf178766759
elvis <cenelvis@gmail.com>
2022-06-17 22:22:16 -0400

    Using { Link }, to preventDefault

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import { Route } from "react-router-dom"; // Needs to be activated with { BrowserRouter } in top layer
 import Welcome from "./pages/Welcome";
-import Product from "./pages/Product";
+import Products from "./pages/Products";
+import MainHeader from "./component/MainHeader"; // <li> of "href" for router "path"
 
 // localhost:3000/welcome
 // localhost:3000/product
+// <main> => only displays the content of the component (if href="path")
 function App() {
   return (
     <div>
-      <Route path="/welcome">
-        <Welcome />
-      </Route>
-      <Route path="/product">
-        <Product />
-      </Route>
+      <MainHeader />
+      <main>
+        <Route path="/welcome">
+          <Welcome />
+        </Route>
+        <Route path="/products">
+          <Products />
+        </Route>
+      </main>
     </div>
   );
 }
